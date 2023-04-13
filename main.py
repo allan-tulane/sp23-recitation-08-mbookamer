7f3bea76c722d4798ab59248bcd306ccb13f7903
mbookamer <79340162+mbookamer@users.noreply.github.com>
2023-04-13 11:36:44 -0500

    Update main.py
    implemented one function

--- main.py
+++ main.py
@@ -14,11 +14,29 @@ def MED(S, T):
         if (S[0] == T[0]):
             return(MED(S[1:], T[1:]))
         else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
+            return(1 + min(MED(S, T[1:]), MED(S[1:], T[1:])))
 
 
 def fast_MED(S, T, MED={}):
     # TODO -  implement memoization
+    #returns only the cost of edit distance 
+    if (S, T) in MED_cache:
+        return MED_cache[(S, T)]
+    elif (T, S) in MED_cahce:
+        return MED_cache[(T, S)]
+    elif (S == ""):
+        len(T) = MED_cache[(S, T)]
+        return len(T)
+    elif (T == ""):
+        len(S) = MED_cache[(S, T)]
+        return len(S)
+    else:
+        if (S[0] == T[0]):
+            MED_cache[(S, T)] = fast_MED(S[1:], T[1:])
+            return MED_cache[(S, T)]
+        else:
+            MED_cache[(S, T)] = (1 + min(fast_MED(S, T[1:]), fast_MED(S[1:], T[1:])))
+            return MED_cache[(S, T)]
     pass
 
 def fast_align_MED(S, T, MED={}):
